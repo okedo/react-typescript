5eb6d64da2bc0ed35ae394cead8b97c2f48b6c74
okedo <okedo@mail.ru>
2018-12-16 23:36:36 +0200

    add button logic fixed

--- src/actions/add-chord.action.ts
+++ src/actions/add-chord.action.ts
@@ -3,7 +3,15 @@ import { IChordPropsModel } from "src/models/chord-block.props.model";
 const ADD_CHORD = "ADD_CHORD";
 
 function addChord(chordList: IChordPropsModel[], selectedChord: IChordPropsModel) {
-    const newChordList = [...chordList, { ...selectedChord }];
+    const newChord = { ...selectedChord };
+    let maxId: number = 0;
+    chordList.map(el => {
+        maxId = parseInt(el.id, 0) >= maxId ? parseInt(el.id, 0) : maxId;
+    });
+    maxId++;
+
+    newChord.id = maxId.toString();
+    const newChordList = [...chordList, { ...newChord }];
     return {
         type: ADD_CHORD,
         payload: newChordList
--- src/components/add-chord-block.component.tsx
+++ src/components/add-chord-block.component.tsx
@@ -1,30 +1,53 @@
 import * as React from "react";
+import { style } from 'typestyle/lib';
 import { IAddChordProps } from './../models/add-chord.props.model';
 import { IChordPropsModel } from './../models/chord-block.props.model';
 
-export class AddChordBlockComponent extends React.Component<IAddChordProps, { selectedChord: IChordPropsModel }> {
+export class AddChordBlockComponent extends React.Component<IAddChordProps, { selectedChord: IChordPropsModel, collapsedList: boolean }> {
+
+    private readonly listElementStyle = style({
+        padding: "10px",
+        border: "solid 1px black",
+        display: "inline-block",
+        width: "100px",
+        textAlign: "center"
+    })
+    private readonly buttonStyle = style({
+        background: "white",
+        border: "solid 1px black"
+    })
 
     private constructor(props: IAddChordProps) {
         super(props);
         this.state = {
-            selectedChord: this.props.chordList[0]
+            selectedChord: this.props.chordList[0],
+            collapsedList: true
         };
     }
 
     public render() {
         return (
             <div>
-                <button onClick={() => this.props.addChord(this.props.chordList, this.state.selectedChord)}>Add Chord</button>
-                <select>
-                    {this.props.basicChords.map((el: IChordPropsModel) => {
-                        return (<option onClick={() => this.handleClick(el)} key={el.id}> {el.name}</option>)
-                    })}
-                </select>
+                <div className={this.listElementStyle} onClick={() => this.toggleListState()}>
+                    {this.state.collapsedList ? (
+                        <div className={this.listElementStyle}>
+                            {this.state.selectedChord.name}
+                        </div>) : this.props.basicChords.map((el: IChordPropsModel) => {
+                            return (<div onClick={() => this.handleClick(el)} key={el.id}> {el.name}</div>)
+                        })}
+                </div>
+                <button className={this.buttonStyle} onClick={() => this.props.addChord(this.props.chordList, this.state.selectedChord)}>Add Chord</button>
             </div>
         );
     }
 
     private handleClick(el: IChordPropsModel): void {
         this.setState({ selectedChord: el });
+    }
+
+    private toggleListState(): void {
+        this.setState({
+            collapsedList: !this.state.collapsedList
+        })
     }
 }
